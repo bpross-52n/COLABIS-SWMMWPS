b9df6b21857a1daf65b931cce747785c8b991207
AmelieHaas <amelie.haas@tu-dresden.de>
2018-04-25 13:32:17 +0200

    Methode MinMax in WPS2 angepasst für alle Knoten

--- WPS2_Auslesen.R
+++ WPS2_Auslesen.R
@@ -7,12 +7,12 @@
 # abstract = Ergebnis der SWMM-Simulation (WPS1) mit/ohne Strassenreinigung;
 
 # wps.in: id = method, type = string,
-# title = Ausgabemodus (MinMax / SingleNode / AllNodes),
-# abstract = minimale/maximale Schadstoffkonzentration / Schadstoffkonzentrationen fuer bestimmten Knoten / Schadstoffkonzentrationen fuer alle Knoten;
+# title = Ausgabemodus (SingleNode / MinMax / AllNodes),
+# abstract = Schadstoffkonzentrationen fuer bestimmten Knoten / minimale/maximale Schadstoffkonzentrationen fuer alle Knoten / Schadstoffkonzentrationen fuer alle Knoten;
 
 # wps.in: id = name, type = string,
 # title = Name des Knoten,
-# abstract = Auswahl eines Knotens im SingleNode-/MinMax-Modus;
+# abstract = Auswahl eines Knotens im SingleNode-Modus;
 
 # wps.in: id = pollutant, type = string,
 # title = Name des Schadstoffes,
@@ -22,8 +22,8 @@
 # Variablen fuer Testlauf in RStudio
 # wps.off;
   binary <- "eschdorf_v6_20141208_new.out"
-  method <- "MinMax"
-  name <- "Out1"
+  method <- "SingleNode"
+  name <- "5"
   pollutant <- "CSB"
 # wps.on;
 
@@ -51,30 +51,26 @@ library(zoo)
 # vollstaendiger Pfad zu .out-File
 binary <- paste0(inp_path,"/",binary)
 
-if(method == "MinMax" || method == "SingleNode"){
+if(method == "SingleNode"){
   
-  # .out-Datei auslesen fuer 1 Knoten (name) und ausgewaehlten Schadstoff (pollutant bzw. var)
+  # .out-Datei auslesen fuer 1 Knoten (name)
+  # und ausgewaehlten Schadstoff (pollutant bzw. var)
   list <- read_out(file=binary, iType=1, vIndex=var, object_name=name)
   title <- names(list) # Name des Knoten (vgl. name)
   var_name <- names(list[[title]]) # Name des Schadstoffes (vgl. pollutant)
   xts <- list[[1]][[1]] # xts-Objekt mit Schadstoffwerten
   
   # Ausgabe: csv-Datei erstellen
-  if(method == "MinMax"){
-    values <- range(xts) # Min-/Max-Werte
-    table <- paste0(title, "_", var_name, "_MinMax.csv") # Name des Output-File
-    write.table(values, file=table, sep=",",dec=".",
-                row.names=c("Min","Max"), col.names=F)
-    
-  } else if (method == "SingleNode"){
-    table <- paste0(title, "_", var_name, ".csv") # Name des Output-File
-    write.zoo(xts, file=table, sep=",",dec=".")
-  }
+  table <- paste0(title, "_", var_name, ".csv") # Name des Output-File
+  write.zoo(xts, file=table, sep=",",dec=".")
   
+}
+
+
+if (method == "MinMax" || method == "AllNodes"){
   
-} else if (method == "AllNodes"){
-  
-  # .out-Datei auslesen fuer alle Knoten (node_names) und ausgewaehlten Schadstoff (pollutant bzw. var)
+  # .out-Datei auslesen fuer alle Knoten (node_names)
+  # und ausgewaehlten Schadstoff (pollutant bzw. var)
   list <- read_out(file=binary, iType=1, object_name=node_names, vIndex=var)
   title <- names(list) # Namen aller Knoten (vgl. node_names)
   var_name <- names(list[[title[1]]]) # Name des Schadstoffes (vgl. pollutant)
@@ -84,9 +80,22 @@ if(method == "MinMax" || method == "SingleNode"){
   }
   colnames(xts) <- node_names
   
-  # Ausgabe: csv-Datei erstellen
-  table <- paste0("AllNodes_", var_name, ".csv") # Name des Output-File
-  write.zoo(xts, file=table, sep=",",dec=".") # Spalten = Knoten, Zeilen = Zeitschritte
+  if(method == "MinMax"){
+    min_max <- range(xts[,1])
+    for(i in 2:ncol(xts)){
+      min_max <- cbind(min_max, range(xts[,i])) # Spalten = Knoten, Zeilen = Min/Max
+    }
+    # Ausgabe: csv-Datei erstellen
+    table <- paste0("AllNodes_", var_name, "_MinMax.csv") # Name des Output-File
+    write.table(min_max, file=table, sep=",", dec=".",
+                row.names=c("Min", "Max"), col.names=node_names)
+  }
+  
+  if(method == "AllNodes"){
+    # Ausgabe: csv-Datei erstellen
+    table <- paste0("AllNodes_", var_name, ".csv") # Name des Output-File
+    write.zoo(xts, file=table, sep=",",dec=".") # Spalten = Knoten, Zeilen = Zeitschritte
+  }
   
 }
 
